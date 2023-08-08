Fix bugs with epsilon calculation. Add early stopping to CBOLoop. Fix bad variable scope in CBOLoop causing issues with |ES| > 1

causal_helper_funcs.py
@@ -2,6 +2,7 @@ from numpy import meshgrid, vstack
 from pandas import DataFrame
 from scipy.spatial import ConvexHull
 import torch
+import numpy as np
 
 # Create subdictionary containing only keys
 def subdict_with_keys(dict: dict, keys: list[str]):
@@ -15,9 +16,16 @@ def bounds_to_hull_points(domains: dict[list[float]]):
     return points
 
 # = (Vol(C(observational_samples)) / Vol(interventional_domain)) * (n / n_max)
+# Discards points with values that may be correct but lie outside of the interventional domain.
 def calculate_epsilon(observational_samples: DataFrame, interventional_domain: dict[list[float]], n_max: int):
     n = observational_samples.shape[0]
-    epsilon = (ConvexHull(observational_samples).volume / 
+
+    df = observational_samples
+    for k in interventional_domain.keys():
+        df = df[ (df[k] >= interventional_domain[k][0])
+               & (df[k] <= interventional_domain[k][1])]    
+        
+    epsilon = (ConvexHull(df).volume / 
                ConvexHull(
                     bounds_to_hull_points(interventional_domain))
                 .volume) * (n / n_max)
@@ -25,6 +33,6 @@ def calculate_epsilon(observational_samples: DataFrame, interventional_domain: d
 
 # Convert DataFrame to torch compatible tensor.
 def df_to_tensor(df: DataFrame):
-    return torch.tensor(df.to_numpy())
+    return torch.tensor(df.to_numpy().astype(np.float64))
 
 

cbo_loop.py
@@ -14,7 +14,7 @@ from botorch.optim import optimize_acqf
 
 def CBOLoop(observational_samples: DataFrame, graph: SCM, exploration_set: list[list[str]], 
             num_steps: int, num_initial_obs: int, num_obs_per_step: int, num_max_allowed_obs: int,
-            interventional_domain: dict[list[float]], type_trial: Literal['min', 'max'], objective_function: dict):
+            interventional_domain: dict[list[float]], type_trial: Literal['min', 'max'], objective_function: dict, early_stopping_iters: int = 0):
     
     num_total_obs: int = num_initial_obs
     D_o: DataFrame = observational_samples[:num_initial_obs]
@@ -22,6 +22,8 @@ def CBOLoop(observational_samples: DataFrame, graph: SCM, exploration_set: list[
     GPs: dict[SingleTaskGP] = {}
     global_optimum: float = -20
     global_optimal_set: str = 'None'
+    global_optimal_value: float = None
+    num_iters_without_improvement: int = 0
 
     # if type_trial == 'min':
     #     global_optimum = min(D_o[graph.output_node])
@@ -46,8 +48,12 @@ def CBOLoop(observational_samples: DataFrame, graph: SCM, exploration_set: list[
         D_i[set_identifier]= DataFrame(columns=s + [graph.output_node])
     
     for t in range(num_steps):
+        if(early_stopping_iters != 0 and num_iters_without_improvement > early_stopping_iters):
+            print("Early stopping reached max num of iters without improvment.")
+            break
+
         print(f"Iteration {t}")
-        print(f"Current global optimum: {global_optimum}")
+        print(f"Current global optimal set-value-result = {global_optimal_set}: {global_optimal_value} -> {global_optimum}")
         uniform = np.random.uniform(0., 1.)
         if t == 0:
             epsilon = 1
@@ -98,12 +104,14 @@ def CBOLoop(observational_samples: DataFrame, graph: SCM, exploration_set: list[
 
             # This is horrendous, fix later
             print(f'Updating D_i for {best_point[0]}...')
+            interventional_data = D_i[best_point[0]]
             interventional_data = concat(
                                 [interventional_data, 
                                  DataFrame([dict(zip(columns, x_values + torch.flatten(new_y).tolist()))])
                             ]) 
 
             print(f'Updating GP posterior for {best_point[0]}...')
+            gp = GPs[best_point[0]]
             gp.set_train_data(inputs=df_to_tensor(interventional_data.loc[:, interventional_data.columns != graph.output_node]),
                               targets=df_to_tensor(interventional_data[graph.output_node]),
                               strict=False)
@@ -119,6 +127,9 @@ def CBOLoop(observational_samples: DataFrame, graph: SCM, exploration_set: list[
             if(global_optimum == None or torch.flatten(new_y)[0] > global_optimum):
                 global_optimum = torch.flatten(new_y)[0]
                 global_optimal_set = best_point[0]
+                global_optimal_value = torch.flatten(best_point[1])[0]
+            else:
+                num_iters_without_improvement += 1
 
     return (global_optimum, global_optimal_set, GPs[global_optimal_set], D_i[global_optimal_set], D_o)
 

testing.py
@@ -6,18 +6,26 @@ from matplotlib import pyplot as plt
 from toy_graph import ToyGraph
 from cbo_loop import CBOLoop
 
-tg = ToyGraph()
-# tg.graph.fit(tg.observational_samples)
+from causal_mean_kernel import CausalMean, CausalRBF
+from botorch.models import SingleTaskGP
+from gpytorch.mlls.exact_marginal_log_likelihood import ExactMarginalLogLikelihood
+from botorch.fit import fit_gpytorch_model
 
-# xs = np.arange(-5, 5, 0.01)
+tg = ToyGraph()
+
+# obs_data = tg.observational_samples[:1000]
+
+# obj_data_x = torch.linspace(-5,5,1000).view(-1,1)
+# obj_data_y = tg.obj_func['X'](obj_data_x)
+
+# tg.graph.fit(obs_data)
+
+# xs = np.linspace(-5,5,1000)
 # ys = [E_output_given_do(interventional_variable=['X'], interventional_value=[x], causal_model=tg.graph) for x in xs]
 
-
+# plt.plot(obj_data_x, obj_data_y)
 # plt.plot(xs, ys)
 # plt.show()
-# obs_data = tg.observational_samples[:100]
-# epsilon = calculate_epsilon(obs_data, tg.interventional_domain, 1000)
-# print(epsilon)
 
 
 (global_optimum, global_optimal_set, gp, D_i, D_o) = CBOLoop(
@@ -25,44 +33,69 @@ tg = ToyGraph()
         graph=tg.graph,
         exploration_set=[['X']],
         num_steps=10,
-        num_initial_obs=100,
+        num_initial_obs=400,
         num_obs_per_step=20,
         num_max_allowed_obs=1000,
         interventional_domain=tg.interventional_domain,
         type_trial='max',
-        objective_function=tg.obj_func)
+        objective_function=tg.obj_func,
+        early_stopping_iters=2)
 
-test_X = torch.linspace(*tg.interventional_domain['X'], 200, dtype=torch.float64)
-obj_func_x = torch.linspace(*tg.interventional_domain['X'], 1000).view(-1,1)
-obj_func_y = tg.obj_func['X'](obj_func_x)
 
-xs = np.arange(-5, 5, 0.01)
-ys = [E_output_given_do(interventional_variable=['X'], interventional_value=[x], causal_model=tg.graph) for x in xs]
 
-f, ax = plt.subplots(1, 1, figsize=(6, 4))
-with torch.no_grad():
-    # compute posterior
-    posterior = gp.posterior(test_X)
-    # Get upper and lower confidence bounds (2 standard deviations from the mean)
-    lower, upper = posterior.mvn.confidence_region()
+# train_X=torch.tensor([[2.0], [0.0], [-4.0], [4.0]], dtype=torch.float64)
+# train_Y = tg.obj_func['X'](torch.tensor([[2.0], [0.0], [-4.0], [4.0]], dtype=torch.float64))
 
-    ax.plot(obj_func_x, obj_func_y, "k")
-    # Plot objective
-    plt.axhline(y=global_optimum, color='r', linestyle='-')
-    ax.plot(xs, ys, "r")
-    # Plot observational data
-    ax.scatter(D_o['X'], D_o['Y'])
-    # Plot training points as black stars
-    ax.plot(gp.train_inputs[0].numpy(), gp.train_targets.numpy(), "k*")
-    # Plot posterior means as blue line
-    ax.plot(test_X.cpu().numpy(), posterior.mean.cpu().numpy(), "b")
-    # Shade between the lower and upper confidence bounds
-    ax.fill_between(
-        test_X.cpu().numpy(), lower.cpu().numpy(), upper.cpu().numpy(), alpha=0.5
-    )
-ax.legend(["Ground truth", "global_optimum", "m(x)", "D_O", "D_I", "GP model", "Confidence"])
-ax.set_title('Causal GP.')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.tight_layout()
-plt.show()
+
+# gp = SingleTaskGP(train_X=train_X, train_Y=train_Y,
+#                             covar_module=CausalRBF(
+#                                 output_variable=tg.graph.output_node,
+#                                 interventional_variable=s,
+#                                 causal_model=tg.graph),
+#                             mean_module=CausalMean(
+#                                 interventional_variable=s,
+#                                 causal_model=tg.graph))
+
+#     # model = SingleTaskGP(train_X=train_X, train_Y=train_Y,
+#     #                                 covar_module=RBFKernel(),
+#     #                                 mean_module=ZeroMean())
+
+# mll = ExactMarginalLogLikelihood(likelihood=gp.likelihood, model=gp)
+# fit_gpytorch_model(mll)
+
+# test_X = torch.linspace(*tg.interventional_domain['X'], 200, dtype=torch.float64)
+# obj_func_x = torch.linspace(*tg.interventional_domain['X'], 1000).view(-1,1)
+# obj_func_y = tg.obj_func['X'](obj_func_x)
+
+# # D_o = tg.observational_samples
+
+# xs = np.arange(-5, 5, 0.01)
+# ys = [E_output_given_do(interventional_variable=['X'], interventional_value=[x], causal_model=tg.graph) for x in xs]
+
+# f, ax = plt.subplots(1, 1, figsize=(6, 4))
+# with torch.no_grad():
+#     # compute posterior
+#     posterior = gp.posterior(test_X)
+#     # Get upper and lower confidence bounds (2 standard deviations from the mean)
+#     lower, upper = posterior.mvn.confidence_region()
+
+#     ax.plot(obj_func_x, obj_func_y, "k")
+#     # Plot objective
+#     #plt.axhline(y=global_optimum, color='r', linestyle='-')
+#     ax.plot(xs, ys, "r")
+#     # Plot observational data
+#     ax.scatter(D_o['X'], D_o['Y'])
+#     # Plot training points as black stars
+#     ax.plot(gp.train_inputs[0].numpy(), gp.train_targets.numpy(), "k*")
+#     # Plot posterior means as blue line
+#     ax.plot(test_X.cpu().numpy(), posterior.mean.cpu().numpy(), "b")
+#     # Shade between the lower and upper confidence bounds
+#     ax.fill_between(
+#         test_X.cpu().numpy(), lower.cpu().numpy(), upper.cpu().numpy(), alpha=0.5
+#     )
+# ax.legend(["Ground truth", "m(x)", "D_O", "D_I", "GP model", "Confidence"])
+# ax.set_title('Causal GP.')
+# plt.xlabel('X')
+# plt.ylabel('Y')
+# plt.tight_layout()
+# plt.show()