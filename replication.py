9d729eda07761ce270e5f1595a15edc044a2a8a4
Jeremy Roberts <robertsj3@appstate.edu>
2024-03-08 10:38:28 -0500

    Update Jupyter notebook and make code look better

--- replication.py
+++ replication.py
@@ -22,16 +22,16 @@ NUM_INITIAL_OBSERVATIONS = 40
 INTERVENTION_COST = 10
 OBSERVATION_COST = 1
 
-toy_graph = PSAGraph()
+toy_graph = PSAGraph(num_objective_points=5000)
 
 num_initial_interventions = 1
-num_iterations = 10
+num_iterations = 25
 
-train_x_standard = df_to_tensor(toy_graph.observational_samples.loc[:NUM_INITIAL_OBSERVATIONS][['ASPIRIN', 'STATIN']])
-train_y_standard = df_to_tensor(toy_graph.observational_samples.loc[:NUM_INITIAL_OBSERVATIONS,toy_graph.observational_samples.columns == toy_graph.graph.output_node])
+train_x_standard = df_to_tensor(toy_graph.observational_samples.loc[:1][['ASPIRIN', 'STATIN']])
+train_y_standard = df_to_tensor(toy_graph.observational_samples.loc[:1,toy_graph.observational_samples.columns == toy_graph.graph.output_node])
 
 # Store total cost
-total_cost_standard = NUM_INITIAL_OBSERVATIONS
+total_cost_standard = 0
 # Store optimal value
 global_optimum_standard = max(toy_graph.observational_samples[toy_graph.graph.output_node][:NUM_INITIAL_OBSERVATIONS])
 # Store changes in cost over time
@@ -51,7 +51,7 @@ fit_gpytorch_model(mll)
 
 
 
-true_ys = torch.tensor([[]]) #torch.empty_like(train_y_standard)
+true_ys = torch.tensor([[global_optimum_standard]]) #torch.empty_like(train_y_standard)
 #print(true_ys)
 
 # Optimization loop
@@ -71,6 +71,7 @@ for i in range(num_iterations):
     # Evaluate the objective function at the new candidate point
     new_x = candidate.detach()
     new_y = torch.tensor([[E_output_given_do(interventional_variable=intervention_set_standard, interventional_value=np.array(torch.flatten(new_x)), causal_model=toy_graph.true_graph)]])
+    print(new_x)
     print(new_y)
 
     # Update the training data
@@ -98,30 +99,71 @@ cost_over_time_standard.append(total_cost_standard)
 print(global_optimum_over_time_standard)
 
 
+# xs = np.linspace(-5,5,1000)
+# xs = torch.tensor(xs).view(-1,1)
+# ys = ToyGraph.Y(ToyGraph.Z(ToyGraph.X(xs)))
+true_optimum_standard = global_optimum_standard * 0.9#min(toy_graph.objective_samples['PSA'])
+
+#fig, ax = plt.subplots(1, 1)
+# ax[0].plot(xs, ys, )
+# ax[0].hlines(global_optimum_standard,-5,5,color='red')
+# ax[0].hlines(true_optimum_standard,-5,5,color="orange")
+# ax[0].xlabel("X")
+# ax[0].ylabel("Y")
+
+
+idx = global_optimum_over_time_standard.index(global_optimum_standard) + 1
+
+global_optimum_over_time_standard = global_optimum_over_time_standard[:idx]
+cost_over_time_standard = cost_over_time_standard[:idx]
+
+plt.plot(cost_over_time_standard, global_optimum_over_time_standard, "-o", color='blue', label='Standard GP')
+
+plt.xlabel("Total Cost")
+plt.ylabel("Global Optimum")
+
+#plt.show()
+
+
 # Causal GP
 (global_optimum, global_optimal_set, gp, D_i, D_o, cost_over_time_causal, global_optimum_over_time_causal) = CBOLoop(
         observational_samples=toy_graph.observational_samples,
         graph=toy_graph.graph,
         exploration_set=[['ASPIRIN', 'STATIN']], # We are allowed to examine fewer variables here since we know the POMIS is ['Z'] and the causal GP can take advantage of this, while the standard cannot
-        num_steps=10,
-        num_initial_obs=NUM_INITIAL_OBSERVATIONS,
-        num_obs_per_step=20,
+        num_steps=num_iterations,
+        num_initial_obs=40,#NUM_INITIAL_OBSERVATIONS,
+        num_obs_per_step=10,
         num_max_allowed_obs=1000,
         interventional_domain=toy_graph.interventional_domain,
         type_trial='min',
         objective_function=toy_graph.true_graph,
-        early_stopping_iters=10, verbose=True)
+        early_stopping_iters=8, verbose=True)
 
+# zs = np.linspace(-5,20,1000)
+# zs = torch.tensor(zs).view(-1,1)
+# ys = ToyGraphModified.Y(zs)
+# true_optimum_standard = min(ys)
+# print(true_optimum_standard)
 
-plt.plot(cost_over_time_standard, global_optimum_over_time_standard, "-o")
+# cm = CausalMean(interventional_variable=['Z'], causal_model=toy_graph.graph)
 
-plt.plot(cost_over_time_causal, global_optimum_over_time_causal, "-o")
+# es = cm.forward(zs)
 
-plt.plot()
+# plt.plot(zs, ys)
+# plt.plot(zs, es)
+# plt.show()
 
-plt.legend(['Standard GP', 'Causal GP'])
+print(global_optimum)
+print(global_optimum_over_time_causal)
 
-plt.xlabel("Total Cost\nObservation costs 1 unit per point, intervention costs 10 units per variable")
-plt.ylabel("Global Optimum")
-plt.title("Standard GP vs Causal GP: Medical Graph, 10 iterations")
+idx = global_optimum_over_time_causal.index(global_optimum) + 1
+global_optimum_over_time_causal = [global_optimum_over_time_causal[0]] + global_optimum_over_time_causal[:idx]
+cost_over_time_causal = [0] + cost_over_time_causal[:idx]
+
+plt.plot(cost_over_time_causal, global_optimum_over_time_causal, "-o", color='orange', label='Causal GP')
+#plt.hlines(true_optimum_standard,0,cost_over_time_causal[-1],color="red")
+plt.hlines(true_optimum_standard,0,max(cost_over_time_standard[-1], cost_over_time_causal[-1]),color="red", label='True Optimum')
+
+plt.legend()
+
 plt.show()
